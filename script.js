890ea45f6bd2a66306696055791a400c140a644e
jk-0-01 <190198221+jk-0-01@users.noreply.github.com>
2026-05-24 22:51:31 +0800

    Add functions to get human and computer choices as well as the function for how each round goes
    getComputerChoice() will generate a random number from 0 to 2 using the
    Math.random() function.
    
    getHumanChoice() will prompt the player for input and return that input.
    
    playRound() will determine who scores during each round as well as
    increment a point for whoever wins a round.

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
-console.log('testing');
+let computerScore = 0;
+let humanScore = 0;
+
+function playGame() {
+    for (let i = 0; i < 5; i++) {
+        playRound();
+    }
+}
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    if (humanChoice == computerChoice) {
+        console.log(`Tie! ${humanChoice} vs ${computerChoice}`)
+    }   // for when player picks "rock"
+    else if (humanChoice == "rock" && computerChoice == "paper") {
+        console.log("You lose! Paper beats rock.");
+        return ++computerChoice;
+    } else if (humanChoice == "rock" && computerChoice == "scissors") {
+        console.log("You win! Rock beats scissors.");
+        return ++humanChoice;
+    }   // for when player picks "paper"
+     else if (humanChoice == "paper" && computerChoice == "rock") {
+        console.log("You win! Paper beats rock.");
+        return ++humanChoice;
+    } else if (humanChoice == "paper" && computerChoice == "scissors") {
+        console.log("You lose! Scissors beats paper.");
+        return ++computerChoice;
+    }   // for when player picks "scissors"
+     else if (humanChoice == "scissors" && computerChoice == "rock") {
+        console.log("You lose! Rock beats scissors.");
+        return ++computerChoice;
+    } else if (humanChoice == "scissors" && computerChoice == "paper") {
+        console.log("You win! Scissors beats paper.");
+        return ++humanChoice;
+    }
+}
+
+function getComputerChoice() {
+    return Math.floor(Math.random() * 3);
+}
+
+function getHumanChoice(humanChoice) {
+    humanChoice = prompt("Your pick: ");
+    return humanChoice;
+}
